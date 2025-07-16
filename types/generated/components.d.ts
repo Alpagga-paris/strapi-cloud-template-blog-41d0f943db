c5151316939f354f03cd361b46bf2eb9991ed6bc
LouisLeGatt <louis.legatt@matters.tech>
2025-07-16 10:15:39 +0200

    fix(ALP-577): editing homepage logo list

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,28 +1,5 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
-export interface LandingComponentsHeroHeader extends Struct.ComponentSchema {
-  collectionName: 'components_landing_components_hero_headers';
-  info: {
-    displayName: 'HeroHeader';
-  };
-  attributes: {
-    content: Schema.Attribute.RichText;
-    button: Schema.Attribute.Component<'landing-components.button', false>;
-  };
-}
-
-export interface LandingComponentsButton extends Struct.ComponentSchema {
-  collectionName: 'components_landing_components_buttons';
-  info: {
-    displayName: 'Button';
-  };
-  attributes: {
-    text: Schema.Attribute.String;
-    outlined: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
-    link: Schema.Attribute.Text;
-  };
-}
-
 export interface SharedSlider extends Struct.ComponentSchema {
   collectionName: 'components_shared_sliders';
   info: {
@@ -85,6 +62,150 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
+export interface LandingComponentsHeroHeader extends Struct.ComponentSchema {
+  collectionName: 'components_landing_components_hero_headers';
+  info: {
+    displayName: 'HeroHeader';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    button: Schema.Attribute.Component<'landing-components.button', false>;
+  };
+}
+
+export interface LandingComponentsButton extends Struct.ComponentSchema {
+  collectionName: 'components_landing_components_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    text: Schema.Attribute.String;
+    outlined: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Text;
+  };
+}
+
+export interface HomepageTestimonial extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_testimonials';
+  info: {
+    displayName: 'Review';
+    description: '';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    review: Schema.Attribute.Text;
+  };
+}
+
+export interface HomepageTestimonialList extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_testimonial_lists';
+  info: {
+    displayName: 'ReviewListAndRate';
+    description: '';
+  };
+  attributes: {
+    reviews: Schema.Attribute.Component<'homepage.testimonial', true>;
+    rate: Schema.Attribute.Decimal;
+  };
+}
+
+export interface HomepageLogos extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_logos';
+  info: {
+    displayName: 'logos';
+  };
+  attributes: {};
+}
+
+export interface HomepageHomepage extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_homepages';
+  info: {
+    displayName: 'Homepage';
+    description: '';
+  };
+  attributes: {
+    bannerList: Schema.Attribute.Component<'homepage.banner-list', false>;
+    clientLogoList: Schema.Attribute.Component<
+      'homepage.client-logo-list',
+      true
+    >;
+    reviewListAndRate: Schema.Attribute.Component<
+      'homepage.testimonial-list',
+      false
+    >;
+    faq: Schema.Attribute.Component<'homepage.faq-list', false>;
+  };
+}
+
+export interface HomepageFaq extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_faqs';
+  info: {
+    displayName: 'SingleQuestion';
+    description: '';
+  };
+  attributes: {
+    question: Schema.Attribute.String;
+    answer: Schema.Attribute.RichText;
+  };
+}
+
+export interface HomepageFaqList extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_faq_lists';
+  info: {
+    displayName: 'FAQList';
+    description: '';
+  };
+  attributes: {
+    questions: Schema.Attribute.Component<'homepage.faq', true>;
+  };
+}
+
+export interface HomepageClientLogoList extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_client_logo_lists';
+  info: {
+    displayName: 'ClientLogoList';
+    description: '';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    link: Schema.Attribute.String;
+    image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+  };
+}
+
+export interface HomepageBanner extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_banners';
+  info: {
+    displayName: 'Banner';
+    description: '';
+  };
+  attributes: {
+    desktopImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    mobileImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    altText: Schema.Attribute.String;
+    link: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageBannerList extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_banner_lists';
+  info: {
+    displayName: 'BannerList';
+    description: '';
+  };
+  attributes: {
+    banners: Schema.Attribute.Component<'homepage.banner', true>;
+  };
+}
+
 export interface ComponentsTrusting extends Struct.ComponentSchema {
   collectionName: 'components_components_trustings';
   info: {
@@ -144,139 +265,29 @@ export interface ComponentsBanner extends Struct.ComponentSchema {
   };
 }
 
-export interface HomepageTestimonial extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_testimonials';
-  info: {
-    displayName: 'Review';
-    description: '';
-  };
-  attributes: {
-    name: Schema.Attribute.String;
-    title: Schema.Attribute.String;
-    review: Schema.Attribute.Text;
-  };
-}
-
-export interface HomepageTestimonialList extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_testimonial_lists';
-  info: {
-    displayName: 'ReviewListAndRate';
-    description: '';
-  };
-  attributes: {
-    reviews: Schema.Attribute.Component<'homepage.testimonial', true>;
-    rate: Schema.Attribute.Decimal;
-  };
-}
-
-export interface HomepageHomepage extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_homepages';
-  info: {
-    displayName: 'Homepage';
-    description: '';
-  };
-  attributes: {
-    bannerList: Schema.Attribute.Component<'homepage.banner-list', false>;
-    clientLogoList: Schema.Attribute.Component<
-      'homepage.client-logo-list',
-      false
-    >;
-    reviewListAndRate: Schema.Attribute.Component<
-      'homepage.testimonial-list',
-      false
-    >;
-    faq: Schema.Attribute.Component<'homepage.faq-list', false>;
-  };
-}
-
-export interface HomepageFaq extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_faqs';
-  info: {
-    displayName: 'SingleQuestion';
-    description: '';
-  };
-  attributes: {
-    question: Schema.Attribute.String;
-    answer: Schema.Attribute.RichText;
-  };
-}
-
-export interface HomepageFaqList extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_faq_lists';
-  info: {
-    displayName: 'FAQList';
-    description: '';
-  };
-  attributes: {
-    questions: Schema.Attribute.Component<'homepage.faq', true>;
-  };
-}
-
-export interface HomepageClientLogoList extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_client_logo_lists';
-  info: {
-    displayName: 'ClientLogoList';
-    description: '';
-  };
-  attributes: {
-    logos: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios',
-      true
-    >;
-  };
-}
-
-export interface HomepageBanner extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_banners';
-  info: {
-    displayName: 'Banner';
-    description: '';
-  };
-  attributes: {
-    desktopImage: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios'
-    >;
-    mobileImage: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios'
-    >;
-    altText: Schema.Attribute.String;
-    link: Schema.Attribute.String;
-  };
-}
-
-export interface HomepageBannerList extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_banner_lists';
-  info: {
-    displayName: 'BannerList';
-    description: '';
-  };
-  attributes: {
-    banners: Schema.Attribute.Component<'homepage.banner', true>;
-  };
-}
-
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'landing-components.hero-header': LandingComponentsHeroHeader;
-      'landing-components.button': LandingComponentsButton;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
       'shared.rich-text': SharedRichText;
       'shared.quote': SharedQuote;
       'shared.media': SharedMedia;
-      'components.trusting': ComponentsTrusting;
-      'components.testimonial': ComponentsTestimonial;
-      'components.testimonial-list': ComponentsTestimonialList;
-      'components.banner': ComponentsBanner;
+      'landing-components.hero-header': LandingComponentsHeroHeader;
+      'landing-components.button': LandingComponentsButton;
       'homepage.testimonial': HomepageTestimonial;
       'homepage.testimonial-list': HomepageTestimonialList;
+      'homepage.logos': HomepageLogos;
       'homepage.homepage': HomepageHomepage;
       'homepage.faq': HomepageFaq;
       'homepage.faq-list': HomepageFaqList;
       'homepage.client-logo-list': HomepageClientLogoList;
       'homepage.banner': HomepageBanner;
       'homepage.banner-list': HomepageBannerList;
+      'components.trusting': ComponentsTrusting;
+      'components.testimonial': ComponentsTestimonial;
+      'components.testimonial-list': ComponentsTestimonialList;
+      'components.banner': ComponentsBanner;
     }
   }
 }
