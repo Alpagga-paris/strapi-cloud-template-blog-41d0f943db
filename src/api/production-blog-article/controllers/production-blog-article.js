f711db543fdebe9c678969c7dcb54cdceaf727cf
Valentin <valentin@alpagga.com>
2025-10-30 10:28:17 +0100

    ✨ feat(ALP-976): Add production-blog-article single type for banner

---
+++ src/api/production-blog-article/controllers/production-blog-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * production-blog-article controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::production-blog-article.production-blog-article');
